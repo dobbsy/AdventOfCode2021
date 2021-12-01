01d507aa8c5f71837ee28388740f08d82cbdf007
Tobias Dötzer <tobias.doetzer@gmx.de>
2021-12-01 16:57:44 +0100

    Added the solution for part 2 of Day 1

--- app/src/main/kotlin/de/tobiasdoetzer/aoc2021/Day1.kt
+++ app/src/main/kotlin/de/tobiasdoetzer/aoc2021/Day1.kt
@@ -12,6 +12,21 @@ fun part1(fileName: String): Int {
     return retVal
 }
 
+fun part2(fileName: String): Int {
+    val depths = File("app/src/main/kotlin/de/tobiasdoetzer/aoc2021", fileName).readLines().map(String::toInt)
+
+    val windowDepths = mutableListOf<Int>()
+
+    for (i in 0..depths.lastIndex - 2) {
+        windowDepths += depths[i] + depths[i + 1] + depths[i + 2]
+    }
+
+    return windowDepths.zipWithNext().count { (first, second) ->
+        second > first
+    }
+}
+
 fun main() {
     println("The solution for part 1 is: ${part1("day1_input.txt")}")
+    println("The solution for part 2 is: ${part2("day1_input.txt")}")
 }
