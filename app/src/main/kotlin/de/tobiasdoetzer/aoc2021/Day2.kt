9263c7a081a632665416f9e26c620bef829d90f3
Tobias Dötzer <tobias.doetzer@gmx.de>
2021-12-02 20:05:10 +0100

    Added the solution for day 2

---
+++ app/src/main/kotlin/de/tobiasdoetzer/aoc2021/Day2.kt
@@ -0,0 +1,51 @@
+package de.tobiasdoetzer.aoc2021
+
+import java.lang.IllegalArgumentException
+
+fun part1(instructions: List<String>): Int {
+    var horizontalPos = 0
+    var depth = 0
+
+    instructions.forEach {
+        val (direction, _magnitude) = it.split(' ')
+        val magnitude = _magnitude.toInt()
+
+        when (direction) {
+            "forward" -> horizontalPos += magnitude
+            "down" -> depth += magnitude
+            "up" -> depth -= magnitude
+            else -> throw IllegalArgumentException("Could not interpret direction \"$direction\"")
+        }
+    }
+
+    return depth * horizontalPos
+}
+
+fun part2(instructions: List<String>): Int {
+    var horizontalPos = 0
+    var depth = 0
+    var aim = 0
+
+    instructions.forEach {
+        val (direction, _magnitude) = it.split(' ')
+        val magnitude = _magnitude.toInt()
+
+        when(direction) {
+            "forward" -> {
+                horizontalPos += magnitude
+                depth += aim * magnitude
+            }
+            "up" -> aim -= magnitude
+            "down" -> aim += magnitude
+            else -> throw IllegalArgumentException("Could not interpret direction \"$direction\"")
+        }
+    }
+
+    return depth * horizontalPos
+}
+
+fun main() {
+    val input = readInput("day2_input.txt")
+    println("The solution of part 1 is: ${part1(input)}")
+    println("The solution of part 2 is: ${part2(input)}")
+}
