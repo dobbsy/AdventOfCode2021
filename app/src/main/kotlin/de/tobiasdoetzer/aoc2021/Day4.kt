11d2f1bcdb461282895c6bc41c60a427a9a414f0
Tobias Dötzer <tobias.doetzer@gmx.de>
2021-12-04 23:32:58 +0100

    Added some documentation to the BinoBoard class of day 4.

--- app/src/main/kotlin/de/tobiasdoetzer/aoc2021/Day4.kt
+++ app/src/main/kotlin/de/tobiasdoetzer/aoc2021/Day4.kt
@@ -41,6 +41,10 @@ private fun part2(boards: MutableList<BingoBoard>, drawnNumbers: List<Int>): Int
     throw IllegalArgumentException()
 }
 
+/**
+ * Instances of this class represent a 5x5 Bingo board
+ * Please note that the constructor is private. To create a new instance of this class, use the [createBoard] function.
+ */
 class BingoBoard private constructor(private val rowsAndColumns: Array<MutableSet<Int>>) {
     private var lastNumber: Int? = null
 
@@ -49,10 +53,20 @@ class BingoBoard private constructor(private val rowsAndColumns: Array<MutableSe
         lastNumber = number
     }
 
+    /**
+     * Creates a new instance of BingoBoard with the exact same state as this
+     */
     fun copy() = BingoBoard(rowsAndColumns)
 
+    /**
+     * Returns true iff at least one row or column of this board is completely marked
+     */
     fun isWinningBoard() = rowsAndColumns.any { it.isEmpty() }
 
+    /**
+     * Creates the score of a winning board.
+     * @throws IllegalStateException if the board is not in a winning state when calculateScore is called
+     */
     fun calculateScore(): Int {
         if (!isWinningBoard()) throw IllegalStateException("Cannot calculate score if the board has not won")
 
@@ -63,6 +77,12 @@ class BingoBoard private constructor(private val rowsAndColumns: Array<MutableSe
     }
 
     companion object {
+        /**
+         * Creates a board based on a list of Strings
+         *
+         * @param[numberStrings] A list with 5 String items. Each item is a String with five numbers,
+         *  separated by whitespace
+         */
         fun createBoard(numberStrings: List<String>): BingoBoard {
             val numbers = numberStrings.map { it.trim().split("\\s+".toRegex()) }.map { list -> list.map(String::toInt) }
             val rowsAndColumns = Array<MutableSet<Int>>(10) { mutableSetOf() }
