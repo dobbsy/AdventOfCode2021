ee03e294afea8b8e1050c092efe1a1f074f5e960
Tobias Dötzer <tobias.doetzer@gmx.de>
2021-12-06 18:31:48 +0100

    Added a new utility function that reads one line of integers from a file in preparation of Day 6

--- app/src/main/kotlin/de/tobiasdoetzer/aoc2021/Utility.kt
+++ app/src/main/kotlin/de/tobiasdoetzer/aoc2021/Utility.kt
@@ -2,8 +2,17 @@ package de.tobiasdoetzer.aoc2021
 
 import java.io.File
 
+const val FILE_PREFIX = "app/src/main/kotlin/de/tobiasdoetzer/aoc2021"
+
 fun readInput(fileName: String): List<String> =
-    File("app/src/main/kotlin/de/tobiasdoetzer/aoc2021", fileName).readLines()
+    File(FILE_PREFIX, fileName).readLines()
 
 fun readInputAsInts(fileName: String): List<Int> =
     readInput(fileName).map(String::toInt)
+
+fun readInputSingleLineAsInts(fileName: String, delimiter: String = ","): List<Int> =
+    readInput(fileName)[0].split(delimiter).map(String::toInt)
+
+fun <E> MutableList<E>.addIfNotNull(item: E?) {
+    if (item != null) add(item)
+}
