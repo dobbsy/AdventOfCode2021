28f57accccb98d8c2949171cd393669e67789d32
Tobias Dötzer <tobias.doetzer@gmx.de>
2021-12-07 21:42:49 +0100

    Added my solution for part 2 of Day 7

--- app/src/main/kotlin/de/tobiasdoetzer/aoc2021/Day7.kt
+++ app/src/main/kotlin/de/tobiasdoetzer/aoc2021/Day7.kt
@@ -12,8 +12,21 @@ private fun part1(positions: List<Int>): Int {
     return result
 }
 
+private fun part2(positions: List<Int>): Int {
+    var result = Int.MAX_VALUE
+    for (i in positions.minOrNull()!!..positions.maxOrNull()!!) {
+        val fuelUsed = positions.sumOf {
+            val distance = abs(i - it)
+            (distance * (distance + 1)) / 2
+        }
+        result = min(result, fuelUsed)
+    }
+    return result
+}
+
 private fun main() {
     val input = readInputSingleLineAsInts("day7_input.txt")
 
     println("The solution of part 1 is: ${part1(input)}")
+    println("The solution of part 2 is: ${part2(input)}")
 }
